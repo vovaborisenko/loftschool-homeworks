99500736e5b2accd415aa6c5b1751ecd85da17d6
Vova Borisenko <vovaborisenko@live.com>
2019-01-22 12:18:09 +0300

    done

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (localStorageKey, initValue) => WrappedComponent => {
+    return class extends Component {
+        saveData = (data) => {
+            save(localStorageKey, data)
+            this.forceUpdate();
+        }
+
+        savedData = () => load(localStorageKey) || initValue;
+
+        render () {
+            return (
+                <WrappedComponent
+                    savedData={this.savedData()}
+                    saveData={this.saveData}
+                    {...this.props}
+                />
+            );
+        }
+    };
+};
 
 export default withLocalstorage;
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -14,24 +14,93 @@ class Todo extends PureComponent {
     return biggest + 1;
   }
 
-  handleChange = event => {};
+  handleChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  };
 
-  createNewRecordByEnter = event => {};
+  createNewRecordByEnter = event => {
+    if(event.key === 'Enter'){
+      this.createNewRecord()
+    }
+  };
 
-  toggleRecordComplete = event => {};
+  toggleRecordComplete = event => {
+    const { savedData, saveData } = this.props;
+    const id = event.target.dataset.todoId;
+    const newData = savedData;        
+    const ndx = newData.findIndex(item => item.id === +id);
 
-  createNewRecord = () => {};
+    newData[ndx].isComplete = !newData[ndx].isComplete;
+    saveData(newData);
+  };
+
+  createNewRecord = () => {
+    const { inputValue } = this.state;
+    const { savedData, saveData } = this.props;
+
+    if (inputValue !== '') {
+      const id = this.getId();
+      const data = [{
+        id: id,
+        text: inputValue,
+        isComplete: false
+      }, ...savedData];
+
+      saveData(data);
+
+      this.setState({
+        inputValue: ''
+      });
+    }
+  };
 
   render() {
-    return;
+    const { savedData } = this.props;
+
+    return (
+      <Card title="Список дел">
+        <div className="todo t-todo-list">
+          {this.renderEmptyRecord()}
+          {savedData.map(item => this.renderRecord(item))}
+        </div>
+      </Card>
+    );
   }
 
   renderEmptyRecord() {
-    return;
+    const {inputValue} = this.state;
+
+    return (
+      <div className="todo-item todo-item-new">
+        <input
+          className="todo-input t-input"
+          placeholder="Введите задачу"
+          onChange={this.handleChange}
+          onKeyPress={this.createNewRecordByEnter}
+          value={inputValue}
+        />
+        <span className="plus t-plus" onClick={this.createNewRecord}>+</span>
+      </div>
+    );
   }
 
   renderRecord = record => {
-    return;
+    const { id, text, isComplete } = record;
+
+    return (
+      <div className="todo-item t-todo" key={id}>
+        <p className="todo-item__text">{text}</p>
+        <span
+          className="todo-item__flag t-todo-complete-flag"
+          data-todo-id={id}
+          onClick={this.toggleRecordComplete}
+        >
+          {isComplete ? '[x]' : '[ ]'}
+        </span>
+      </div>
+    );
   };
 }
 
